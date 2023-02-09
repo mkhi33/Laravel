3c0c064f643971da61669534776cca8ad81110cc
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 13:40:34 -0600

    Crear sidebar para mostrar categorias

---
+++ quiosco/react-quiosco/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { categorias } from '../data/categorias'
+import Categoria from './Categoria'
+const Sidebar = () => {
+  return (
+    <aside className="md:w-72">
+        <div className="p-4">
+            <img className="w-40" src="img/logo.svg" />
+        </div>
+        <div className="mt-10">
+            {categorias.map( categoria => (
+                <Categoria key={categoria.id} categoria={categoria} />
+            ))}
+        </div>
+        <div className='my-1 py-3'>
+            <button
+                type='button'
+                className='text-center bg-red-500 w-full p-3 font-bold truncate'
+            >Cancelar Orden</button>
+        </div>
+    </aside>
+  )
+}
+
+export default Sidebar
