7bc7329a28731f28284b1366e14f404947679cf1
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 11:26:06 -0600

    Implementando Outlet para ver el componente hijo

--- quiosco/react-quiosco/src/layouts/Layout.jsx
+++ quiosco/react-quiosco/src/layouts/Layout.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      Layout
+      <Outlet />  
+    </div>
   )
 }
 
