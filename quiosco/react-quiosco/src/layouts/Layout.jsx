884f53d6e06e01763a3cfcf9f91f0b8e68aaca96
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 13:42:07 -0600

    Mostrar sidebar, productos, resumen en el layout

--- quiosco/react-quiosco/src/layouts/Layout.jsx
+++ quiosco/react-quiosco/src/layouts/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
-
+import Sidebar from "../components/Sidebar"
+import Resumen from "../components/Resumen"
 const Layout = () => {
   return (
-    <div>
-      Layout
-      <Outlet />  
+    <div className="md:flex">
+      <Sidebar />
+      <main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-3">
+        <Outlet />  
+      </main>
+      <Resumen />
     </div>
   )
 }
