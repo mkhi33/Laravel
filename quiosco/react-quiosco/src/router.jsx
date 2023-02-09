210b4b0355860492197347b47d272fd580ec9cec
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 11:28:44 -0600

    Agregando Login como hijo de AuthLayout

--- quiosco/react-quiosco/src/router.jsx
+++ quiosco/react-quiosco/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import AuthLayout from './layouts/AuthLayout';
 import Layout from './layouts/Layout';
 import Inicio from './views/Inicio';
+import Login from './views/Login';
 const router = createBrowserRouter([
     { 
         path:'/',
@@ -10,7 +11,13 @@ const router = createBrowserRouter([
             {index: true, element: <Inicio />},
         ] 
     },
-    { path:'/auth', element: <AuthLayout /> },
+    {
+        path:'/auth',
+        element: <AuthLayout />,
+        children: [
+            {index: true, element: <Login />},
+        ]
+    },
 ])
 
 export default router;
