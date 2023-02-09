e6caa1bc12ea22298d95ea7d7b201e98d87292e3
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 12:06:43 -0600

    Crear el AuthLayout

--- quiosco/react-quiosco/src/layouts/AuthLayout.jsx
+++ quiosco/react-quiosco/src/layouts/AuthLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
-    <div>
-
-      <Outlet />
-    </div>
+    <main className="max-w-4xl mx-auto mt-5 md:mt-10 flex flex-col md:flex-row items-center">
+      <img className="max-w-xs" src="../public/img/logo.svg" alt="Imagen logotipo" />
+      <div className="p-2 w-full">
+        <Outlet />
+      </div>
+    </main>
   )
 }
 
