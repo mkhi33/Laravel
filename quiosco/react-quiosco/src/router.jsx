099b954b2eb02767d531cebc2dabb0a4c0d1f4ac
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-09 11:25:26 -0600

    Agregando componente hijo para el layout

--- quiosco/react-quiosco/src/router.jsx
+++ quiosco/react-quiosco/src/router.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import AuthLayout from './layouts/AuthLayout';
 import Layout from './layouts/Layout';
+import Inicio from './views/Inicio';
 const router = createBrowserRouter([
-    { path:'/', element: <Layout /> },
+    { 
+        path:'/',
+        element: <Layout />, 
+        children: [
+            {index: true, element: <Inicio />},
+        ] 
+    },
     { path:'/auth', element: <AuthLayout /> },
 ])
 
