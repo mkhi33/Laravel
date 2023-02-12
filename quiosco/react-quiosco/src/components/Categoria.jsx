f04cc2c6db2491c74da5ea96b9c43bab3c2eecd2
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-12 09:37:23 -0600

    Seleccionar categoría

--- quiosco/react-quiosco/src/components/Categoria.jsx
+++ quiosco/react-quiosco/src/components/Categoria.jsx
@@ -1,12 +1,14 @@
+import useQuiosco from "../hooks/useQuiosco"
 
 
 const Categoria = ({categoria}) => {
     const { icono, nombre, id  } = categoria
+    const { categoriaActual, handleClickCategoria } = useQuiosco()
   return (
-    <div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer">
+    <button type="button" onClick={ () => handleClickCategoria(id)} className={`flex items-center gap-4 border w-full p-3 ${categoriaActual.id === id ? 'bg-amber-400' : 'bg-white'} hover:bg-amber-400 cursor-pointer`}>
         <img className="w-12" src={`/img/icono_${icono}.svg`} alt={`Imagen ${nombre}`}/>   
         <p className="text-lg font-bold cursor-pointer truncate">{nombre}</p> 
-    </div>
+    </button>
   )
 }
 
