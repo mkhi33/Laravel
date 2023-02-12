bad0897aa2079934bb899e0d21f2133596fb769e
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-12 09:39:26 -0600

    Filtrar productos de la categoría actual

--- quiosco/react-quiosco/src/views/Inicio.jsx
+++ quiosco/react-quiosco/src/views/Inicio.jsx
@@ -1,9 +1,14 @@
 import Producto from '../components/Producto'
-import { productos } from '../data/productos'
+import useQuiosco from '../hooks/useQuiosco'
+import { productos as data } from '../data/productos'
 const Inicio = () => {
+  const { categoriaActual } = useQuiosco()
+
+  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+  
   return (
     <>
-      <h1 className='text-4xl font-black'>Inicio</h1>
+      <h1 className='text-4xl font-black'>{categoriaActual?.nombre}</h1>
       <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuación</p>
       <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
         {productos.map(producto => (
