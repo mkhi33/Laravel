d048e6d0dd55d8ef5909dd974fc38608116afe40
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-12 11:25:12 -0600

    Mostrar resumen del pedido

--- quiosco/react-quiosco/src/components/Resumen.jsx
+++ quiosco/react-quiosco/src/components/Resumen.jsx
@@ -1,8 +1,34 @@
-
+import useQuiosco from "../hooks/useQuiosco"
+import ResumenProducto from "./ResumenProducto"
 
 const Resumen = () => {
+  const { pedido } = useQuiosco()
   return (
-    <div className="w-72">Resumen</div>
+    <div className="w-72 h-screen overflow-y-scroll p-5">
+      <h1 className="text-4xl font-black">Mi pedido</h1>
+      <p className="text-lg my-5">Aquí podrás ver el resumen y total de tu pedido</p>
+      <div className="py-10">
+        { pedido?.length === 0 ? ( 
+          <p className="text-center text-2xl">No hay elementos en tu pedido todavía</p>
+        ) : (
+          pedido.map( producto => (
+            <ResumenProducto key={`resumen-producto-${producto.id}`} producto={producto} />
+          ) )
+        )}
+      </div>
+      <p className="text-xl mt-10">
+        Total: {''}
+      </p>
+      <form className="w-full">
+        <div className="mt-5">
+          <input 
+            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer"
+            type="submit"
+            value="Confirmar Pedido" 
+          />
+        </div>
+      </form>
+    </div>
   )
 }
 
