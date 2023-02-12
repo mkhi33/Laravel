c8838802b4202029f18178eaf9171b9d6fcd23d9
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-12 09:36:33 -0600

    Implementando QuioscoProvider

--- quiosco/react-quiosco/src/main.jsx
+++ quiosco/react-quiosco/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import './index.css'
+import QuioscoProvider from './context/QuioscoProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider
-      router={router}
-    >
-
-    </RouterProvider>
+      <QuioscoProvider>
+      
+        <RouterProvider router={router} ></RouterProvider>
+      </QuioscoProvider>
   </React.StrictMode>,
 )
