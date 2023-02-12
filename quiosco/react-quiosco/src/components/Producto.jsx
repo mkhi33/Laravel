db0bc8b44165afd545aeecee5de4fcef03f47046
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-02-12 09:38:11 -0600

    Seleccionar producto actual y mostrar modal

--- quiosco/react-quiosco/src/components/Producto.jsx
+++ quiosco/react-quiosco/src/components/Producto.jsx
@@ -1,14 +1,21 @@
 import { formatearDinero } from "../helpers";
+import useQuiosco from "../hooks/useQuiosco";
 
 const Producto = ({ producto }) => {
     const { nombre, imagen , precio} = producto;
+    const { handleClickModal, handleSetProducto } = useQuiosco()
   return (
     <div className="border p-3 shadow bg-white">
         <img className="w-full" src={`/img/${imagen}.jpg`} alt={`Imagen ${nombre}`} />
         <div className="p-5">
             <h3 className="text-2xl font-bold">{nombre}</h3>
             <p className="mt-5 font-black text-4xl text-amber-500">{formatearDinero(precio)}</p>
-            <button type="button" className="bg-indigo-600 hover:bg-indigo-800 text-white mt-5 p-3 uppercase font-bold w-full" >Agregar</button>
+            <button 
+            onClick={ () => {
+              handleClickModal();
+              handleSetProducto(producto);
+            } } 
+            type="button" className="bg-indigo-600 hover:bg-indigo-800 text-white mt-5 p-3 uppercase font-bold w-full" >Agregar</button>
         </div>
 
     </div>
